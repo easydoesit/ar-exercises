794bfb40aeba8ac8a7759898e10dc9dea1d1dbad
easydoesit <michael.grills@gmail.com>
2023-04-04 15:28:33 +0000

    add exercise_5.rb

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum('annual_revenue')
+
+puts "Company Total Revenue: $#{@total_revenue}"
+
+@average_revenue = Store.average('annual_revenue')
+
+puts "Stores Average Revenue: $#{@average_revenue}"
+
+@stores_over_1M = Store.where("annual_revenue > 1000000").size
+
+puts "#Stores With over 1M revenue: #{@stores_over_1M}"
